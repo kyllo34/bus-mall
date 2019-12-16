e045065b77c4881016e56b2f36016b1226650abc
kyllo34 <kyllo34@gmail.com>
2019-12-16 15:24:30 -0800

    added function that generates 3 random images

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 // set variable equal to image tag
 var imageEl = document.getElementsByTagName('img');
+
+var totalClicks = -1;
 // object constructor for all images
 var allImages = [];
 function Image(name, imageUrl){
@@ -10,6 +12,7 @@ function Image(name, imageUrl){
   this.imageViews = 0;
   allImages.push(this);
 }
+// We know what the first three images are
 
 // Create images
 new Image('bag', 'img/bag.jpg');
@@ -18,22 +21,34 @@ new Image('bathroom', 'img/bathroom.jpg');
 new Image('boots', 'img/boots.jpg');
 new Image('breakfast', 'img/breakfast.jpg');
 new Image('bubblegum', 'img/bubblegum.jpg');
-new Image('bus', 'img/bus.jpg');
 new Image('chair', 'img/chair.jpg');
 new Image('cthulu', 'img/cthulhu.jpg');
-new Image('bag', 'img/bag.jpg');
-
+new Image('dog-duck', 'img/dog-duck.jpg');
+new Image('dragon', 'img/dragon.jpg');
+new Image('pen', 'img/pen.jpg');
+new Image('pet-sweep', 'img/pet-sweep.jpg');
+new Image('scissors', 'img/scissors.jpg');
+new Image('shark', 'img/shark.jpg');
+new Image('sweep', 'img/sweep.png');
+new Image('tauntaun', 'img/tauntaun.jpg');
+new Image('unicorn', 'img/unicorn.jpg');
+new Image('water-can', 'img/water-can.jpg');
+new Image('wine-glass', 'img/wine-glass.jpg');
 
 // function for when there is an event
-function imageWasClicked(event) {
-  console.log('image was clicked');
-
+function displayImages(event) {
+  totalClicks++;
+  // first image rendered = random number * (imageEl -1)
+  imageEl[1].src = allImages[Math.floor(Math.random() * allImages.length)].imageUrl;
+  imageEl[2].src = allImages[Math.floor(Math.random() * allImages.length)].imageUrl;
+  imageEl[3].src = allImages[Math.floor(Math.random() * allImages.length)].imageUrl;
 }
 
-
+// We call the imagewasclicked function first to generate 3 random images
+displayImages();
 
 // create an event listener for images
 // make it listen to all images displayed except the first image as it is the logo
-for (var i = 1; i < imageEl.length; i ++) {
-  imageEl[i].addEventListener('click', imageWasClicked);
+for (var i = 1; i < imageEl.length; i++) {
+  imageEl[i].addEventListener('click', displayImages);
 }
