ec291a63881d9b9cacb5bc7e71f81b818778f8e8
kyllo34 <kyllo34@gmail.com>
2019-12-16 14:42:05 -0800

    added object constructor and objects

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 'use strict';
 // set variable equal to image tag
-var imageEl = document.getElementById('1');
+var imageEl = document.getElementsByTagName('img');
+// object constructor for all images
+var allImages = [];
+function Image(name, imageUrl){
+  this.name = name;
+  this.imageUrl = imageUrl;
+  this.imageClicks = 0;
+  this.imageViews = 0;
+  allImages.push(this);
+}
+
+// Create images
+new Image('bag', 'img/bag.jpg');
+new Image('banana', 'img/banana.jpg');
+new Image('bathroom', 'img/bathroom.jpg');
+new Image('boots', 'img/boots.jpg');
+new Image('breakfast', 'img/breakfast.jpg');
+new Image('bubblegum', 'img/bubblegum.jpg');
+new Image('bus', 'img/bus.jpg');
+new Image('chair', 'img/chair.jpg');
+new Image('cthulu', 'img/cthulhu.jpg');
+new Image('bag', 'img/bag.jpg');
+
+
 // function for when there is an event
 function imageWasClicked(event) {
   console.log('image was clicked');
+
 }
 
 
+
 // create an event listener for images
-imageEl.addEventListener('click', imageWasClicked);
+// make it listen to all images displayed except the first image as it is the logo
+for (var i = 1; i < imageEl.length; i ++) {
+  imageEl[i].addEventListener('click', imageWasClicked);
+}
