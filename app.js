549bd44fab99447042fd3e25a472352ce2593fe0
kyllo34 <kyllo34@gmail.com>
2019-12-16 14:16:24 -0800

    added event listener on app

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 'use strict';
-console.log('app linked');
+// set variable equal to image tag
+var imageEl = document.getElementById('1');
+// function for when there is an event
+function imageWasClicked(event) {
+  console.log('image was clicked');
+}
+
+
+// create an event listener for images
+imageEl.addEventListener('click', imageWasClicked);
